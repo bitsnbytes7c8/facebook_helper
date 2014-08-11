c9fc412565cf4c883af77106e4ff4bfd0f9b6427
Arjun S Nath <arjun.sn7c8@gmail.com>
2014-08-11 18:23:01 +0530

    Added comments

--- fql.py
+++ fql.py
@@ -1,6 +1,9 @@
+# Calls fql query given the query and access token
+
 import urllib
 import json
 
+#Given fql query and access_token returns the output string as a json
 def get_json(query, access_token):
   params = urllib.urlencode({'q':query, 'access_token':access_token});
   url = "https://graph.facebook.com/fql?"+params;
--- userDetails.py
+++ userDetails.py
@@ -1,7 +1,11 @@
+#Returns user details given user id
+# Requires to be present in the same directory as the facebook-sdk
+
 import facebook
 import json
 import fql;
 
+#Returns user's name given user id and access token
 def get_name_from_id(uid, token):
   query = "SELECT name FROM user WHERE uid="+str(uid);
   name_json = fql.get_json(query, token);
@@ -9,6 +13,7 @@ def get_name_from_id(uid, token):
   for n in name:
     return n['name'];
 
+#Returns users details given the detail required, user id and access_token
 def get_detail_from_id(detail, uid, token):
   query = "SELECT" + detail + "FROM user WHERE uid="+str(uid);
   detail_json = fql.get_json(query, token);
